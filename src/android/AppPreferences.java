48f7387be469393099367ed5a18d094df0be8262
an-rohangovekar <rohan.g@applicationnexus.com>
2015-12-30 15:18:35 +0530

    Update AppPreferences.java

--- src/android/AppPreferences.java
+++ src/android/AppPreferences.java
@@ -3,6 +3,7 @@ package me.apla.cordova;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
